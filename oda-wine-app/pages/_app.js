90aff924bf501a4146f8521e34dbf784128a81e4
alaj07030 <alaj07030@gmail.com>
2026-05-19 13:43:14 +0100

    initial upload
    initial upload

---
+++ oda-wine-app/pages/_app.js
@@ -0,0 +1,10 @@
+// pages/_app.js
+import '../styles/globals.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  )
+}
